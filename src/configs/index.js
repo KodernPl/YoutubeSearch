6c556656d319c44707a5eaf30fa9bd38598b8c19
Krzysztof Kuziel <krzysztofkuziel84@gmai.com>
2016-12-26 16:55:20 +0100

    Add API_KEY

--- src/configs/index.js
+++ src/configs/index.js
@@ -1,3 +1,3 @@
 // GET API v3 KEY FROM console.developers.google.com
 // replace API_KEY with your own
-export const YT_API_KEY = '';
+export const YT_API_KEY = 'AIzaSyAuQCVeNfKhtRk9KlChQPT1nO27DPO_5Ss';
